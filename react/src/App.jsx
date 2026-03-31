a1138ab01fc95b576691b310d6193ac7edcde421
JangYeseon <yeseon04@ewhain.net>
2026-03-31 14:14:59 +0900

    토이프로젝트

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,30 @@
-
-
-function App() {
+import { useState } from "react";
+const Card = () => {
+  const [comment, setComment] = useState("");
+  const [number, setNumber] = useState(0);
 
   return (
     <>
-      
+    <h1> 토이프로젝트 </h1>
+    <div>
+      <textarea
+      onChange={(e) => setComment(e.target.value)}
+      placeholder="텍스트"
+      required
+      ></textarea>
+    </div>
+    <button disabled={comment.length === 0} type="submit">
+      버튼
+    </button>
+    <br/>
+        <label>
+        Text input: <input name="myInput" defaultValue="" />
+      </label>
+      <input type ="number" value={number}
+      onChange={(e) => setNumber(e.target.value)}
+    />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default  Card;
