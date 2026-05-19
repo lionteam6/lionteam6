539815cbd7c5a8fee6b81156ba41ba73d9121346
JangYeseon <yeseon04@ewhain.net>
2026-05-19 13:55:38 +0900

    과제 수정

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/logo.png";
 import bell from "../assets/notifications.svg";
 import search from "../assets/search.svg";
 
-import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../store/seAuthStore.Js";
 
 import { toast } from "react-toastify";
 
